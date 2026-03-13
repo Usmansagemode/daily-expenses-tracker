bc9e03dc014d59fed50dfc067a67b27694e582c6
Usmansagemode <usmansagemode@gmail.com>
2026-03-13 13:24:30 -0400

    removing profile icon

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,19 +1,10 @@
 "use client";
 
-import { LogOut, Search, Settings, User } from "lucide-react";
+import { Search } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { SidebarTrigger } from "../ui/sidebar";
 
 import { CommandPalette } from "./CommandPalette";
@@ -47,32 +38,6 @@ const Navbar = () => {
           </kbd>
         </Button>
         <ModeToggle />
-
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            {" "}
-            <Avatar>
-              <AvatarImage src="https://media.licdn.com/dms/image/v2/D4E03AQGJ-e_tzceJxQ/profile-displayphoto-shrink_400_400/B4EZR2EBOcG0Ak-/0/1737147553020?e=1762387200&v=beta&t=JjAlu_LFbsyd5e89kUj48qKg-V_bAAl-7aTpSLPS3qE" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent sideOffset={10}>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <User className="mr-2 h-[1.2rem] w-[1.2rem]" />
-              Profile
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Settings className="mr-2 h-[1.2rem] w-[1.2rem]" />
-              Settings
-            </DropdownMenuItem>
-            <DropdownMenuItem variant="destructive">
-              <LogOut className="mr-2 h-[1.2rem] w-[1.2rem]" />
-              Logout
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
       </div>
     </nav>
   );
